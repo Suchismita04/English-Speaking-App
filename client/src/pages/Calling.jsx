17aecad32717de68f421886b9a24e5fa80272e6f
TanmoyWGT <tanmoy.webgen@gmail.com>
2025-07-27 01:39:29 +0530

    add pages

---
+++ client/src/pages/Calling.jsx
@@ -0,0 +1,9 @@
+const Calling = () => {
+  return (
+    <>
+      This is the Calling page.
+    </>
+  );
+};
+
+export default Calling;
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 
+
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+   
     </Routes>
   );
 };
