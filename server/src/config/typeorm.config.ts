28d47017221af22c53a6a26f9d51aae3d28a0158
wasim <wasimmiddya21@gmail.com>
2025-10-04 23:35:40 +0530

    feat(env): add sample environment variables for application configuration feat(typeorm): update database host configuration to use environment variables

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import { ConfigService } from "@nestjs/config";
+import { config } from "process";
 
 export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => {
 
   return {
     type: 'mssql',
-    host: 'localhost',
+    host: configService.get<string>('DB_HOST'),
     port: parseInt(configService.get<string>('DB_PORT') ?? '1433', 10),
     username: configService.get<string>('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
