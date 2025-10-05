3f8bf4ff5f75c86489f6e8630aaa54f0c45f06e1
Suchismita04 <suchismitachatterjee13@gmail.com>
2025-10-05 19:10:55 +0530

    add specific room controller

--- server/src/session-membership/session-membership.controller.ts
+++ server/src/session-membership/session-membership.controller.ts
@@ -1,8 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Body, Controller,Get, Post,Param } from "@nestjs/common";
 
 
 
-@Injectable()
+@Controller("session-membership-details")
 export class SessionMembershipDetailController{
-    
+
+    //join in a specific room
+    @Post('joinRoom')
+    joinRoom(@Body() body:{userId:string,roomId:string}){
+
+    }
+
+
+     //leave from a specific room
+    @Post('leaveRoom')
+    leaveRoom(@Body() body:{userId:string,roomId:string}){
+
+    }
+
+
+    //get all members in a room
+    @Get(':roomId/members')
+    showAllMembers(@Param('roomId') roomId:string){
+
+    }
 }
