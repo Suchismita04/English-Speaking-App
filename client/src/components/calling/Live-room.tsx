0a69e1855f17be005271e5fe7fd17ca1df09c8e0
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-10-10 00:15:10 +0530

    add LiveRoom, Profile, Progress, and RandomCall components

---
+++ client/src/components/calling/Live-room.tsx
@@ -0,0 +1,9 @@
+const LiveRoom = () => {
+  return (
+    <div>
+      <h1>Live Room</h1>
+    </div>
+  );
+};
+
+export default LiveRoom;
---
+++ client/src/components/calling/Random-call.tsx
@@ -0,0 +1,9 @@
+const RandomCall = () => {
+  return (
+    <div>
+      <h1>Random Call</h1>
+    </div>
+  );
+};
+
+export default RandomCall;
