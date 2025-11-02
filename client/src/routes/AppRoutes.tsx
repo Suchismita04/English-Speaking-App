4e39b9d46ca56baced90981ac5ec12bcb4d0dcc9
Tanmoy70 <tanmoykanrar1@gmail.com>
2025-11-03 00:42:03 +0530

    refactor: update AppRoutes to include commented ProtectedRoute for future authentication handling

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,12 +2,20 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Calling from '../pages/Calling';
 import NotFound from '../pages/NotFound';
+// import ProtectedRoute from '../components/common/ProtectedRoute';
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/connect" element={<Calling />} />
+      <Route
+        path="/connect"
+        element={
+          // <ProtectedRoute>
+            <Calling />
+          // </ProtectedRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
