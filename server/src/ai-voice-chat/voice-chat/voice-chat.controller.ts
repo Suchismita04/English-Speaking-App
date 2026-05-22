a2335780a337c5719a032b1ecba03fd24b901e38
Suchismita04 <matainja0146@gmail.com>
2026-05-22 15:26:03 +0530

    add web socket gateway for real time connection

--- server/src/ai-voice-chat/voice-chat/voice-chat.controller.ts
+++ server/src/ai-voice-chat/voice-chat/voice-chat.controller.ts
@@ -1,56 +1,59 @@
-import {
-  Body,
-  Controller,
-  Post,
-  Req,
-  Res,
-  UploadedFile,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { VoiceChatService } from './voice-chat.service';
-import { diskStorage } from 'multer';
-import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guards';
-import { TtsService } from '../tts/tts.service';
 
-@Controller('voice-chat')
-export class VoiceChatController {
-  constructor(
-    private readonly voiceService: VoiceChatService,
-    private readonly ttsService: TtsService,
-  ) {}
-  @Post()
-  @UseGuards(JwtAuthGuard)
-  @UseInterceptors(
-    FileInterceptor('file', {
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          const fileName = Date.now() + '-' + file.originalname;
-          cb(null, fileName);
-        },
-      }),
-    }),
-  )
-  async handleVoiceController(
-    @UploadedFile() file: Express.Multer.File,
-    @Body('sessionId') sessionId: string,
-    @Req() req: any,
-    @Res() res: any,
-  ) {
-    const response = await this.voiceService.handleVoice(
-      file.path,
-      req.user.userId,
-      sessionId,
-    );
-    //for tts service
-    const audioBuffer = await this.ttsService.textToSpeech(response.clearMsg);
-    res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('X-Session-Id', response.sessionId);
-    res.setHeader('X-User-Text', encodeURIComponent(response.userText));
-    res.setHeader('X-AI-Text', encodeURIComponent(response.clearMsg));
+// ............this file is for testing purpose.................
 
-    return res.send(audioBuffer);
-  }
-}
+// import {
+//   Body,
+//   Controller,
+//   Post,
+//   Req,
+//   Res,
+//   UploadedFile,
+//   UseGuards,
+//   UseInterceptors,
+// } from '@nestjs/common';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { VoiceChatService } from './voice-chat.service';
+// import { diskStorage } from 'multer';
+// import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guards';
+// import { TtsService } from '../tts/tts.service';
+
+// @Controller('voice-chat')
+// export class VoiceChatController {
+//   constructor(
+//     private readonly voiceService: VoiceChatService,
+//     private readonly ttsService: TtsService,
+//   ) {}
+//   @Post()
+//   @UseGuards(JwtAuthGuard)
+//   @UseInterceptors(
+//     FileInterceptor('file', {
+//       storage: diskStorage({
+//         destination: './uploads',
+//         filename: (req, file, cb) => {
+//           const fileName = Date.now() + '-' + file.originalname;
+//           cb(null, fileName);
+//         },
+//       }),
+//     }),
+//   )
+//   async handleVoiceController(
+//     @UploadedFile() file: Express.Multer.File,
+//     @Body('sessionId') sessionId: string,
+//     @Req() req: any,
+//     @Res() res: any,
+//   ) {
+//     const response = await this.voiceService.handleVoice(
+//       file.path,
+//       req.user.userId,
+//       sessionId,
+//     );
+//     //for tts service
+//     const audioBuffer = await this.ttsService.textToSpeech(response.clearMsg);
+//     res.setHeader('Content-Type', 'audio/mpeg');
+//     res.setHeader('X-Session-Id', response.sessionId);
+//     res.setHeader('X-User-Text', encodeURIComponent(response.userText));
+//     res.setHeader('X-AI-Text', encodeURIComponent(response.clearMsg));
+
+//     return res.send(audioBuffer);
+//   }
+// }
---
+++ server/src/ai-voice-chat/voice-chat/voice-chat.gateway.ts
@@ -0,0 +1,44 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { TtsService } from '../tts/tts.service';
+import { VoiceChatService } from './voice-chat.service';
+// import { User } from 'src/user/entities/user.entity';
+
+@WebSocketGateway({ cors: true })
+export class VoiceChatGateway {
+  constructor(
+    private voiceService: VoiceChatService,
+    private ttsService: TtsService,
+    // private readonly user:User
+  ) {}
+
+  @SubscribeMessage('voice_chunk')
+  async handleVoice(
+    @MessageBody() payload: any,
+    @ConnectedSocket() client: any,
+  ) {
+    const { audio, sessionId } = payload;
+    const userId = payload.userId;
+    
+ // send the user voice chuncks to get the text response
+    const response = await this.voiceService.handleVoiceBuffer(
+      audio,
+      userId,
+      sessionId,
+    );
+
+    //for tts part
+    const audioBuffer = await this.ttsService.textToSpeech(response.clearMsg);
+
+    client.emit('voice_response', {
+      audio: audioBuffer,
+      sessionId: response.sessionId,
+      userText: response.userText,
+      aiText: response.clearMsg,
+    });
+  }
+}
