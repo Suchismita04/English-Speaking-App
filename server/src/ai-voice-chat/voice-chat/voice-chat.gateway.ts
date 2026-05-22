05c12b2f070d63addfa62291935ef0057be0cf1b
Suchismita04 <suchismitachatterjee13@gmail.com>
2026-05-22 16:13:35 +0530

    add:implement auth for web socket

--- server/src/ai-voice-chat/voice-chat/voice-chat.gateway.ts
+++ server/src/ai-voice-chat/voice-chat/voice-chat.gateway.ts
@@ -3,36 +3,55 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  OnGatewayConnection,
 } from '@nestjs/websockets';
+import { JwtService } from '@nestjs/jwt';
 import { TtsService } from '../tts/tts.service';
 import { VoiceChatService } from './voice-chat.service';
-// import { User } from 'src/user/entities/user.entity';
 
 @WebSocketGateway({ cors: true })
-export class VoiceChatGateway {
+export class VoiceChatGateway implements OnGatewayConnection {
   constructor(
     private voiceService: VoiceChatService,
     private ttsService: TtsService,
-    // private readonly user:User
+    private jwtService: JwtService,
   ) {}
 
+  //to get the auth data 
+  handleConnection(client: any) {
+    const token = client.handshake.auth?.token;
+    if (!token) {
+      console.log('No token, disconnecting...');
+      client.disconnect();
+      return;
+    }
+
+    try {
+      const decoded = this.jwtService.verify(token);
+      client.user = decoded;
+      console.log('User connected:', decoded);
+    } catch (err) {
+      console.log('Invalid token');
+      client.disconnect();
+    }
+  }
+
+  //main voice connection
   @SubscribeMessage('voice_chunk')
   async handleVoice(
     @MessageBody() payload: any,
     @ConnectedSocket() client: any,
   ) {
+    const userId = client.user.id;
+
     const { audio, sessionId } = payload;
-    const userId = payload.userId;
-    
- // send the user voice chuncks to get the text response
+
     const response = await this.voiceService.handleVoiceBuffer(
       audio,
       userId,
       sessionId,
     );
-    
 
-    //for tts part
     const audioBuffer = await this.ttsService.textToSpeech(response.clearMsg);
 
     client.emit('voice_response', {
